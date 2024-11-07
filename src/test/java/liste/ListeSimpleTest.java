a26c99625f36bfc6ebb62c8502bae97f24e4d9e4
TheoPhongIA <theophong@outlook.fr>
2024-11-07 13:45:27 +0100

    Exercice 3.3

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -2,6 +2,8 @@ package liste;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
